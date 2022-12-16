afd94e6fe661fc5e348b1c783c30c7fddfed1d87
Jaroslaw Mazurek <jaroslawmazurek1995@gmail.com>
2022-12-16 20:56:32 +0100

    addem Card, Expense, ExpenseDate, mofied App, ExpenseItem

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -16,35 +16,21 @@ function App() {
     },
     {
       id: "e3",
-      title: "New TV",
+      title: "Car",
       amount: 799.49,
-      date: new Date(2021, 2, 28),
+      date: new Date(2021, 3, 27),
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
-      amount: 450,
+      amount: 294.67,
       date: new Date(2021, 5, 12),
     },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
