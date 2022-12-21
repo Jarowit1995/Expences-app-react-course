beccd5183c78cb24ee06147f3ef886951ebedeab
Jaroslaw Mazurek <jaroslawmazurek1995@gmail.com>
2022-12-21 20:09:00 +0100

    useState - różnice wykorzystania

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Expenses from "./components/Expenses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 const App=()=> {
   const expenses = [
     {
@@ -29,8 +29,9 @@ const App=()=> {
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense ></NewExpense>
       <Expenses items={expenses}></Expenses>
+      
     </div>
   );
 }
